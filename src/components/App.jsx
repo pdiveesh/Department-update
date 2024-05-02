3993fdf90c8f28d00d37efaf46690ac67bf58db8
pdiveesh <diveeshpoli18@gmail.com>
2024-05-02 06:40:59 +0530

    add notification

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import MyForm from './Form';
 import AddDepartment from './AddDepartment';
+import NotificationContent from './NotificationContent';
 
 function App() {
   const [selectedButton, setSelectedButton] = useState(null);
@@ -15,7 +16,7 @@ function App() {
       case 'Add/View a user':
         return <MyForm></MyForm>;
       case 'Send a Notification':
-        return <div>Send a Notification content</div>;
+        return <NotificationContent />;
       case 'User Payment section':
         return <div>User Payment section content</div>;
       case 'Call back Requests':
